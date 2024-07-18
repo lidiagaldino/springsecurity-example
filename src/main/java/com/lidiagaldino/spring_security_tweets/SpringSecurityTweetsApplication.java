e24714e3554562014630318a76cb528a0f52798b
lidiagaldino <lidiagabrielly14@gmail.com>
2024-07-18 13:08:07 -0300

    Spring setup

---
+++ src/main/java/com/lidiagaldino/spring_security_tweets/SpringSecurityTweetsApplication.java
@@ -0,0 +1,13 @@
+package com.lidiagaldino.spring_security_tweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTweetsApplication.class, args);
+	}
+
+}
