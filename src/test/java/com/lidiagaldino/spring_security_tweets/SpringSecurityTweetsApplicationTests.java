ce58ca76fd87db9f174c7257d6a439fd3089555a
lidiagaldino <lidiagabrielly14@gmail.com>
2024-07-18 13:05:08 -0300

    Spring setup

---
+++ src/test/java/com/lidiagaldino/spring_security_tweets/SpringSecurityTweetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lidiagaldino.spring_security_tweets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTweetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
